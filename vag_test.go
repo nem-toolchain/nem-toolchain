67575daf58a889412853ba4ac369656a0c35a55f
r8d8 <ckryvomaz@gmail.com>
2017-09-16 17:52:47 +0300

    Lack of unit tests for account gen Added draft

---
+++ vag_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+	"encoding/hex"
+	"fmt"
+)
+
+func TestAccountGenerator(t *testing.T) {
+	acc := "TCGQQKN5HED66OQ67Z2F7GGWZ66DWVBFJUW6F5WC"
+	pub, _ := hex.DecodeString("c342dbf7cdd3096c4c3910c511a57049e62847dd5030c7e644bc855acc1fd626")
+
+	account := toAccount(pub, TestnetId)
+	fmt.Println(account)
+	if account != acc {
+		t.Fatalf("Mismatched account: expected %v but received %v", acc, account)
+	}
+}
