26d052078a08dd4e12c9753624bb4b0f826c5403
r8d8 <ckryvomaz@gmail.com>
2017-09-18 21:16:33 +0300

    Useless example file Removed

--- vanity_test.go
+++ vanity_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAccountGenerator(t *testing.T) {
+func TestToAccount(t *testing.T) {
 	var assert = assert.New(t)
 	acc := "TCGQQKN5HED66OQ67Z2F7GGWZ66DWVBFJUW6F5WC"
 	pub, _ := hex.DecodeString("c342dbf7cdd3096c4c3910c511a57049e62847dd5030c7e644bc855acc1fd626")
