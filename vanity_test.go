cdaafdfb87dcbec1b0060c951a8101ce5826d864
r8d8 <ckryvomaz@gmail.com>
2017-09-19 12:05:26 +0300

    Lack of cli implementation Added draft

--- vanity_test.go
+++ vanity_test.go
@@ -1,4 +1,4 @@
-package example
+package vanity
 
 import (
 	"encoding/hex"
@@ -12,10 +12,56 @@ func TestToAccount(t *testing.T) {
 	acc := "TCGQQKN5HED66OQ67Z2F7GGWZ66DWVBFJUW6F5WC"
 	pub, _ := hex.DecodeString("c342dbf7cdd3096c4c3910c511a57049e62847dd5030c7e644bc855acc1fd626")
 
-	account, err := toAccount(pub, TestnetId)
+	account, err := ToAccount(pub, TestnetId)
 	assert.NoError(err)
 
 	if account != acc {
 		t.Fatalf("Mismatched account: expected %v but received %v", acc, account)
 	}
+}
+
+func TestToChainId(t *testing.T) {
+	var assert = assert.New(t)
+
+	_, err := ToChainId("mainnet")
+	assert.NoError(err)
+	_, err = ToChainId("main")
+	assert.NoError(err)
+	_, err = ToChainId("0x68")
+	assert.NoError(err)
+	_, err = ToChainId("68")
+	assert.NoError(err)
+	_, err = ToChainId("testnet")
+	assert.NoError(err)
+	_, err = ToChainId("test")
+	assert.NoError(err)
+	_, err = ToChainId("0x98")
+	assert.NoError(err)
+	_, err = ToChainId("98")
+	assert.NoError(err)
+	_, err = ToChainId("mijin")
+	assert.NoError(err)
+	_, err = ToChainId("0x60")
+	assert.NoError(err)
+	_, err = ToChainId("60")
+	assert.NoError(err)
+
+	_, err = ToChainId("mainNet")
+	assert.Error(err)
+	_, err = ToChainId("Main")
+	assert.Error(err)
+	_, err = ToChainId("TE")
+	assert.Error(err)
+	_, err = ToChainId("testN")
+	assert.Error(err)
+	_, err = ToChainId("0x100")
+	assert.Error(err)
+	_, err = ToChainId("0x")
+	assert.Error(err)
+	_, err = ToChainId("")
+	assert.Error(err)
+	_, err = ToChainId("miiijni")
+	assert.Error(err)
+	_, err = ToChainId("101010")
+	assert.Error(err)
 }
