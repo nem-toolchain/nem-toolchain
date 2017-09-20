446e5d1dbcd58ae17556d6fc30cf8fe49450cff0
Dmitry Ulanov (aka dulanov) <dulanov@gmail.com>
2017-09-20 12:36:42 +0200

    Broken package dependencies
    Fix it

--- cmd/nem/main.go
+++ cmd/nem/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/r8d8/nem-toolchain"
+	"github.com/r8d8/nem-toolchain/pkg/vanity"
 	"github.com/urfave/cli"
 )
 
@@ -28,13 +27,20 @@ func main() {
 	}
 
 	app.Action = func(c *cli.Context) error {
-		chainId, err := vanity.ToChainId(chainStr)
-		if err != nil {
-			fmt.Println(err)
-			return err
+		var chainId byte
+
+		switch chainStr {
+		case "mijin", "0x60", "60":
+			chainId = vanity.MijinId
+		case "mainnet", "main", "0x68", "68":
+			chainId = vanity.MainnetId
+		case "testnet", "test", "0x98", "98":
+			chainId = vanity.TestnetId
+		default:
+			panic("Unknown chain")
 		}
 
-		acc, err := vanity.GenerateAccount(chainId)
+		acc, err := vanity.GenAddress(chainId)
 		if err != nil {
 			fmt.Println(err)
 			return err
