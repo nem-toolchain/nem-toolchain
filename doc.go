335c33cc3ceec7ebb86948585f6834cfda12e33f
Dmitry Ulanov (aka dulanov) <dulanov@gmail.com>
2017-09-22 01:19:59 +0200

    golink warning
    Fix it

--- doc.go
+++ doc.go
@@ -1,10 +1,13 @@
+// Copyright 2017 The nem-toolchain project authors. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+
 // Package nem is a set of packages that provide many tools for Nem blockchain.
 //
 // nem-toolchain library contains the following packages:
 //
 // The core package contains core domain model.
 //
-// The keypair package responses for private, public and address account subjects.
+// The keypair responses for account's private/public crypto keys.
 //
 // The vanity package implements a bundle of vanity address generators.
 package nem
