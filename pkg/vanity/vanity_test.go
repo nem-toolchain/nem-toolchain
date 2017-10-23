3f42eb032cbae3106e698ca2eccdd5bed7063c04
Dmitry Ulanov (aka dulanov) <dulanov@gmail.com>
2017-10-24 01:40:02 +0200

    Update unitests coverage

---
+++ pkg/vanity/vanity_test.go
@@ -0,0 +1,24 @@
+// Copyright 2017 The nem-toolchain project authors. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+
+package vanity
+
+import (
+	"testing"
+
+	"strings"
+
+	"github.com/nem-toolchain/nem-toolchain/pkg/core"
+	"github.com/nem-toolchain/nem-toolchain/pkg/keypair"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStartSearch(t *testing.T) {
+	rs := make(chan keypair.KeyPair)
+	s, _ := NewPrefixSelector(core.Testnet, "TA")
+	go StartSearch(core.Testnet, s, rs)
+	select {
+	case p := <-rs:
+		assert.True(t, strings.HasPrefix(p.Address(core.Testnet).String(), "TA"))
+	}
+}
